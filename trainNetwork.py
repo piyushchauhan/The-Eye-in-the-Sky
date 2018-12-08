c52287095d1c06fac27c8c6ff07773dcfad86944
Piyush Chauhan <chauhanpiyush@hotmail.com>
2018-12-08 17:37:44 +0530

    Added flow of the program

---
+++ trainNetwork.py
@@ -0,0 +1,9 @@
+# TODO Load Data
+# TODO Adding Indices as channels
+# TODO Convert RGB to n_classes channels
+# TODO Reflection Padding
+# TODO Slice into the patchs
+# TODO Data augumentation
+# TODO Put into the model model.fit
+# TODO Stitch all patchs
+# TODO Model summary
